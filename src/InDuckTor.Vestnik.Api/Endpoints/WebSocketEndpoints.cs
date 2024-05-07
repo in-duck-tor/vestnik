6ddc93d141fafafbe27120791388b5ae71a06f15
Vlad Suhankov <vladsuhankov@gmail.com>
2024-05-07 23:01:42 +0700

    добавил тестовый эндпоинт для проверки авторизации

--- src/InDuckTor.Vestnik.Api/Endpoints/WebSocketEndpoints.cs
+++ src/InDuckTor.Vestnik.Api/Endpoints/WebSocketEndpoints.cs
@@ -1,6 +1,7 @@
 using InDuckTor.Shared.Models;
 using InDuckTor.Shared.Strategies;
 using InDuckTor.Vestnik.Features.Account;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InDuckTor.Vestnik.Api.Endpoints;
@@ -18,10 +19,19 @@ public static class WebSocketEndpoints
             .RequireAuthorization();
 
         groupBuilder.MapGet("hueta", Hueta);
+
+        groupBuilder.MapGet("some", Some)
+            .WithOpenApi()
+            .RequireAuthorization();
     }
 
     private static void Hueta([FromServices] IExecutor<IHueta, Unit, Unit> hueta, CancellationToken ct)
     {
         hueta.Execute(new(), ct);
+    }
+
+    private static Ok<string> Some([FromQuery(Name = "access_token")] string accessToken)
+    {
+        return TypedResults.Ok("");
     }
 }
